7b8cff489ac7acb8b65da36a93e05d5bdc276004
dunsiu <934750772@qq.com>
2017-11-22 23:59:14 +0800

    修改订单金额方式

--- web/src/views/checkstand/Edit.vue
+++ web/src/views/checkstand/Edit.vue
@@ -24,7 +24,7 @@
 <div class="layout-body">
 	<div class="container-body">
 		<Row>
-			<Col span="4">
+			<Col span="4" offset="1">
 			    <Form label-position="top" class="order-info">
 					<FormItem label="订单金额："><span>￥168.00</span></FormItem>
 					<FormItem label="优惠信息：">
@@ -37,7 +37,7 @@
 					<FormItem label="待收金额："><span class="strong">￥-58.80</span></FormItem>
 			    </Form>
 			</Col>
-			<Col span="10" offset="1">
+			<Col span="14" offset="1">
 				<Form v-model="orderInfo" label-position="top">
 					<FormItem label="入住人：">
 						<Row :gutter="8">
@@ -68,18 +68,19 @@
 			        </FormItem>
 					<FormItem label="入住：">
 						<Row :gutter="8">
-							<Col span="6">
-								{{room.number}}&nbsp;({{room.type_name}})
+							<Col span="4">
+								{{date}}
 							</Col>
-							<Col span="10">
-								<Input v-model="orderInfo.price" :placeholder="placeholder"></Input>
+							<Col span="3">
+								{{room.type_name}}
 							</Col>
-						</Row>
-						<Row :gutter="8" class="mt">
-							<Col span="6">
-								{{date}}入住
+							<Col span="2">
+								{{room.number}}
 							</Col>
-							<Col span="5">
+							<Col span="3">
+								￥{{room.default_price}}
+							</Col>
+							<Col span="4">
 								<Select placeholder="入住方式" v-model="orderInfo.type" @on-change="timeChooseShow">
 					                <Option value="1" v-if="room.allow_hour_room">全日房</Option>
 					                <Option value="2">钟点房</Option>
@@ -88,13 +89,21 @@
 							<Col span="5" v-show="orderInfo.type==1">
 					             <InputNumber v-model="orderInfo.dayNum" :max="360" :min="1" :step="1"></InputNumber><span class="icon-ml">晚</span>
 							</Col>
-							<Col span="5" v-show="orderInfo.type==2">
+							<Col span="4" v-show="orderInfo.type==2">
 					             <TimePicker v-model="orderInfo.hour" type="time" placement="bottom-end" placeholder="时间选择"></TimePicker>
+							</Col>
+						</Row>
+						<Row class="mt">
+							<Col span="20">
+								<Switch v-model="change_price" size="small" :true-value="1" :false-value="0">
+	                            </Switch>
+								<span class="icon-ml">修改订单金额：</span>
+								<Input v-model="orderInfo.price" :placeholder="placeholder" style="width: 160px;" :disabled="change_price"></Input>
 							</Col>
 						</Row>
 			        </FormItem>
 					<FormItem label="消费：">
-						<Row :gutter="8" class="mt" v-for="(pay,i,p) in orderInfo.pay">
+						<Row :gutter="8" v-for="(pay,i,p) in orderInfo.pay">
 							<Col span="5">
 								<Select v-model="pay.expenseItem" placeholder="消费项">
 					                <Option v-for="(expanseItem,ei) in expanseItems" :value="expanseItem.key">{{expanseItem.value}}</Option>
@@ -158,6 +167,7 @@ export default{
 		return {
 			room:{},
 			date:'',
+			change_price: 0,
 			placeholder:'',
 			orderInfo: {
 			    type: 1,
