14e649fae31bf9c5ca49342f9065fb3de3ccd2cb
dunsiu <934750772@qq.com>
2017-12-29 14:52:23 +0800

    微调样式

--- web/src/views/tourist/Index.vue
+++ web/src/views/tourist/Index.vue
@@ -10,7 +10,10 @@
         height: 100%;
         color: #ffffff;
         overflow: hidden;
-        background: url('/src/images/bg.jpg') 50% 0% no-repeat;
+        background: url('/src/images/bg.jpg');
+        background-attachment: fixed;
+        background-repeat: no-repeat;
+        background-size: cover;
         .banner-flyover{
             width: 100%;
             height: 100%;
